2fd8825e52eb2268c4ab1fe87e5ecc8eb60f7988
cnlucke <cristy.lucke@gmail.com>
2018-01-17 17:35:23 -0500

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,44 @@
+class Show < ActiveRecord::Base
+  #highest_rating: this method should return the highest value in the ratings column. hint: if there is a #minimum Active Record method, might there be a #maximum method?
+
+  def self.highest_rating
+    Show.order(rating: :desc).limit(1)[0].rating
+  end
+
+  #most_popular_show: this method should return the show with the highest rating. hint: use the #highest_rating method as a helper method.
+
+  def self.most_popular_show
+    Show.order(rating: :desc).limit(1)[0].name
+  end
+
+  #lowest_rating: returns the lowest value in the ratings column.
+
+  def self.lowest_rating
+    Show.order(:rating).limit(1)[0].rating
+  end
+
+  #least_popular_show: returns the show with the lowest rating.
+
+  def self.least_popular_show
+    Show.order(:rating).limit(1)[0].name
+  end
+
+  #ratings_sum: returns the sum of all of the ratings.
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  #popular_shows: returns an array of all of the shows that have a rating greater than 5. hint: use the #where Active Record method.
+
+  def self.popular_shows
+
+  end
+
+  #shows_by_alphabetical_order: returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the #order Active Record method.
+
+  def self.shows_by_alphabetical_order
+
+  end
+
+end
